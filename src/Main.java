23bb9ed91b3a2be2d2159168f2494bde9faf7c10
Konstantin Dimitrov <1konstantin.dimitrov@gmail.com>
2022-10-25 19:40:02 +0300

    Create new person.

--- src/Main.java
+++ src/Main.java
@@ -3,5 +3,10 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+
+        String name = scanner.nextLine();
+        int age = Integer.parseInt(scanner.nextLine());
+
+        Person person = new Person(name, age);
     }
 }
