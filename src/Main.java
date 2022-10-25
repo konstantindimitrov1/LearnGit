b4d650cc3be683f3eac334eb5c165871f29a4a9f
Konstantin Dimitrov <103933804+konstantindimitrov1@users.noreply.github.com>
2022-10-25 19:38:37 +0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -3,5 +3,7 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        
+        String randomInput = scanner.nextLine();
     }
 }
